bda0537f67043d604cbfedf5601039045e9416a8
Sai Demo <saidemo@saisaga.com>
2023-08-14 02:38:27 +0700

    udpate NetworkPlayer

--- Assets/_Data/Game/GameManager.cs
+++ Assets/_Data/Game/GameManager.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using Tashi.NetworkTransport;
 using Unity.Netcode;
+using Unity.Services.Lobbies;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
     public NetworkManager networkManager;
     public TashiNetworkTransport NetworkTransport => NetworkManager.Singleton.NetworkConfig.NetworkTransport as TashiNetworkTransport;
+    public float _nextHeartbeat;
+    public float _nextLobbyRefresh;
 
     protected override void Start()
     {
         base.Start();
         this.GameStart();
+    }
+
+    void Update()
+    {
+        this.BattleUpdate();
     }
 
     protected override void LoadComponents()
@@ -36,11 +42,47 @@ public class GameManager : Singleton<GameManager>
         this.ReceiveIncomingDetail();
     }
 
-
     protected virtual void ReceiveIncomingDetail()
     {
         Debug.LogWarning("Receive Incoming Detail");
         var incomingSessionDetails = IncomingSessionDetails.FromUnityLobby(LobbyManager.Instance.lobby);
         NetworkTransport.UpdateSessionDetails(incomingSessionDetails);
+    }
+
+    protected async void BattleUpdate()
+    {
+        if (!LobbyManager.Instance.IsReady()) return;
+
+        if (Time.realtimeSinceStartup >= this._nextHeartbeat && LobbyManager.Instance.isHost)
+        {
+            _nextHeartbeat = Time.realtimeSinceStartup + 15;
+            await LobbyService.Instance.SendHeartbeatPingAsync(LobbyManager.Instance.lobbyId);
+        }
+
+        if (Time.realtimeSinceStartup >= _nextLobbyRefresh)
+        {
+            this._nextLobbyRefresh = Time.realtimeSinceStartup + 2;
+            this.LobbyUpdating();
+        }
+    }
+
+    protected virtual async void LobbyUpdating()
+    {
+        var outgoingSessionDetails = NetworkTransport.OutgoingSessionDetails;
+
+        var updatePlayerOptions = new UpdatePlayerOptions();
+        if (outgoingSessionDetails.AddTo(updatePlayerOptions))
+        {
+            await LobbyService.Instance.UpdatePlayerAsync(LobbyManager.Instance.lobbyId, LobbyManager.Instance.profileName, updatePlayerOptions);
+        }
+
+        if (LobbyManager.Instance.isHost)
+        {
+            var updateLobbyOptions = new UpdateLobbyOptions();
+            if (outgoingSessionDetails.AddTo(updateLobbyOptions))
+            {
+                await LobbyService.Instance.UpdateLobbyAsync(LobbyManager.Instance.lobbyId, updateLobbyOptions);
+            }
+        }
     }
 }
--- Assets/_Data/Lobby/LobbyUpdate.cs
+++ Assets/_Data/Lobby/LobbyUpdate.cs
@@ -2,25 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Tashi.NetworkTransport;
 using Unity.Netcode;
+using Unity.Services.Lobbies;
 using UnityEngine;
 
 public class LobbyUpdate : LobbyAbstract
 {
     public TashiNetworkTransport NetworkTransport => NetworkManager.Singleton.NetworkConfig.NetworkTransport as TashiNetworkTransport;
 
-    protected override void Start()
-    {
-        base.Start();
-        //this.LobbyStart();
-    }
 
-    protected virtual async void ReceiveIncomingDetail()
-    {
-        if (NetworkTransport.SessionHasStarted) return;
 
-        Debug.LogWarning("Receive Incoming Detail");
 
-        var incomingSessionDetails = IncomingSessionDetails.FromUnityLobby(this.lobbyManager.lobby);
-        NetworkTransport.UpdateSessionDetails(incomingSessionDetails);
-    }
 }
